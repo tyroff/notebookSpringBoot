dcc8979568d3ebed133b48727e6719d7d995c3d2
Kiryl Vinakurau <Kiryl_Vinakurau@epam.com>
2021-02-12 11:59:44 +0300

    Init

---
+++ src/main/java/com/epam/vinokurov/notebookSpringBoot/NotebookSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.epam.vinokurov.notebookSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/vinokurov/notebookSpringBoot/NotebookSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.vinokurov.notebookSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
